cf829662e45b253dacaf974605c5fddd9bf84536
Jeffry Gonzalez <jeff@hypertheory.com>
2023-03-06 16:13:04 -0500

    done with API for right now

--- instructor/StatusSolution/Status/Program.cs
+++ instructor/StatusSolution/Status/Program.cs
@@ -1,15 +1,47 @@
-﻿using Status;
+﻿using Marten;
+using Status;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
+// we configure "services" - Entities, Values, Services
+var connectionString = "host=localhost;database=status_dev;username=postgres;password=TokyoJoe138!;port=5432";
+builder.Services.AddMarten(options =>
+{
+    options.Connection(connectionString);
+    if (builder.Environment.IsDevelopment())
+    {
+        options.AutoCreateSchemaObjects = Weasel.Core.AutoCreate.All;
+    }
+
+});
 
 var app = builder.Build();
 
-app.MapGet("/status", () =>
+app.MapGet("/status", async (IDocumentSession db) =>
 {
-    // get the data from a database, or whatever..
-    var statusMessage = new StatusMessage(Guid.NewGuid(), "Looks Good", DateTimeOffset.Now);
-    return Results.Ok(statusMessage);
+    var response = await db.Query<StatusMessage>()
+    .OrderByDescending(sm => sm.When)
+    .FirstOrDefaultAsync();
+
+    if (response == null)
+    {
+        return Results.Ok(new StatusMessage(Guid.NewGuid(), "No Status to Report", DateTimeOffset.Now));
+    }
+    else
+    {
+
+        return Results.Ok(response);
+    }
+});
+
+app.MapPost("/status", async (StatusChangeRequest request, IDocumentSession db) =>
+{
+    // save this in the database 
+    var messageToSave = new StatusMessage(Guid.NewGuid(), request.Message, DateTimeOffset.Now);
+    db.Store<StatusMessage>(messageToSave); // here's the list of work to do.
+    await db.SaveChangesAsync(); // do it!
+    return Results.Ok(messageToSave);
 });
 
 app.Run(); // "Block" 
--- instructor/StatusSolution/Status/StatusMessage.cs
+++ instructor/StatusSolution/Status/StatusMessage.cs
@@ -1,5 +1,7 @@
 ﻿
 namespace Status;
 
+// "Models"
 public  record StatusMessage(Guid Id, string Message, DateTimeOffset When);
 
+public record StatusChangeRequest(string Message);
