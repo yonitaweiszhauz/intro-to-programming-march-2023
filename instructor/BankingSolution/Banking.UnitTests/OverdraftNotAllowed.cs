8a8e745f2dffd76151cb1a8d000decbba818d583
Jeffry Gonzalez <jeff@hypertheory.com>
2023-03-07 16:31:27 -0500

    Fixes #93993     [Fact]     public void BadBehaviorOverdraftIsAllowed()     {         var account = new BankAccount();
            account.Withdraw(account.GetBalance() + .01M);
    
            Assert.Equal(-.01M, account.GetBalance());
        }

---
+++ instructor/BankingSolution/Banking.UnitTests/OverdraftNotAllowed.cs
@@ -0,0 +1,29 @@
+﻿
+using Banking.Domain;
+
+namespace Banking.UnitTests;
+
+public class OverdraftNotAllowed
+{
+    [Fact]
+    public void BadBehaviorOverdraftIsAllowed()
+    {
+        var account = new BankAccount();
+
+        account.Withdraw(account.GetBalance() + .01M);
+
+        Assert.Equal(-.01M, account.GetBalance());
+    }
+
+    [Fact]
+    public void OverdraftDoesNotDecreaseBalance()
+    {
+        // Given
+        var account = new BankAccount();
+        var openingBalance = account.GetBalance();
+
+        account.Withdraw(account.GetBalance() + .01M);
+
+        Assert.Equal(openingBalance, account.GetBalance());
+    }
+}
