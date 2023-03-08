86a9b22f68e4db5f0eedc2378cb9b63abc813477
Jeffry Gonzalez <jeff@hypertheory.com>
2023-03-08 14:10:27 -0500

    Done with single digit

---
+++ instructor/stringcalculator-wed-8/StringCalculator.cs
@@ -0,0 +1,11 @@
+﻿
+namespace StringCalculator;
+
+public class StringCalculator
+{
+
+    public int Add(string numbers)
+    {
+        return numbers == "" ? 0 : int.Parse(numbers); // slime!
+    }
+}
---
+++ instructor/stringcalculator-wed-8/StringCalculatorTests.cs
@@ -0,0 +1,27 @@
+﻿
+namespace StringCalculator;
+
+public class StringCalculatorTests
+{
+    [Fact]
+    public void EmptyStringReturnsZero()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("");
+
+        Assert.Equal(0, result);
+    }
+    [Theory]
+    [InlineData("4",4)]
+    [InlineData("5",5)]
+    [InlineData("1242",1242)]
+    public void SingleDigits(string numbers, int expected)
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add(numbers);
+
+        Assert.Equal(expected, result);
+    }
+}
