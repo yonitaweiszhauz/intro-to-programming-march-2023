15d14b825de7e3b7073181352566bcae54882340
Yonita Weiszhauz <yonita.weiszhauz@gmail.com>
2023-03-08 16:01:01 -0500

    End of day Wednesday 3/8

---
+++ STringCalculator-wed-8/stringcalculator-master/StringCalculator.cs
@@ -0,0 +1,30 @@
+﻿namespace StringCalculator;
+
+public class StringCalculator
+{
+    private int _sum = 0;
+    public int Add(string number1)
+    {
+        return number1 == "" ? 0 : int.Parse(number1);
+    }
+
+    public int AddTwo(string number1, string number2)
+    {
+        return _sum = int.Parse(number1) + int.Parse(number2);
+    }
+
+    public int AddMany(string[] numbers)
+    {
+        int[] intNumbers = Array.ConvertAll(numbers, n => int.Parse(n));
+        foreach(int number in intNumbers)
+        {
+            _sum += number;
+        }
+        return _sum;
+    }
+
+    public int getSum(string number1, string number2)
+    {
+        return _sum;
+    }
+}
---
+++ STringCalculator-wed-8/stringcalculator-master/StringCalculatorTests.cs
@@ -0,0 +1,48 @@
+﻿namespace StringCalculator;
+
+public class StringCalculatorTests
+{
+    [Fact]
+    public void EmptyStringReturnsZero()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("");
+
+        Assert.Equal(0, result);
+    }
+
+    [Theory]
+    [InlineData("4", 4)]
+    [InlineData("5", 5)]
+    public void SingleDigits(string numbers, int expected)
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add(numbers);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("4", "5", 9)]
+    [InlineData("7", "6", 13)]
+    public void TwoDigits(string number1, string number2, int expected)
+    {
+        var calculator = new StringCalculator();
+        var result = calculator.AddTwo(number1, number2);
+        Assert.Equal(expected, result);
+    }
+
+    // #2
+    [Theory]
+    string[] nums = { "4", "5" };
+    [InlineData(9, nums)]
+    // [InlineData("7", "6", 13)]
+    public static int unknownNumberAmount(int expected, params string[] numbers)
+    {
+        var calculator = new StringCalculator();
+        var result = calculator.AddMany(numbers);
+        Assert.Equal(expected, result);
+    }
+}
