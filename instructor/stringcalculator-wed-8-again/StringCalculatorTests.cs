04aadeecc263dc0c8c02fb51bf6bf2e69b8b16b7
Jeffry Gonzalez <jeff@hypertheory.com>
2023-03-08 16:00:56 -0500

    done

---
+++ instructor/stringcalculator-wed-8-again/StringCalculatorTests.cs
@@ -0,0 +1,62 @@
+﻿
+namespace StringCalculator;
+
+public class StringCalculatorTests
+{
+
+    private StringCalculator _calculator;
+
+    public StringCalculatorTests()
+    {
+        _calculator = new StringCalculator();
+    }
+
+    [Fact]
+    public void EmptyStringReturnsZero()
+    {
+        
+
+        var result = _calculator.Add("");
+
+        Assert.Equal(0, result);
+    }
+
+    [Theory]
+    [InlineData("1", 1)]
+    [InlineData("2", 2)]
+    [InlineData("108", 108)]
+    public void SingleDigits(string numbers, int expected)
+    {
+
+        var result = _calculator.Add(numbers);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("1,2", 3)]
+    [InlineData("2,3", 5)]
+    [InlineData("10,20",30 )]
+    [InlineData("10,108", 118)]
+    public void TwoDigits(string numbers, int expected)
+    {
+
+        var result = _calculator.Add(numbers);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("1\n2", 3)]
+    [InlineData("1\n10", 11)]
+    [InlineData("99\n5", 104)]
+    [InlineData("99\n5,1", 105)]
+   
+    public void NewLineDelimeters(string numbers, int expected)
+    {
+
+        var result = _calculator.Add(numbers);
+
+        Assert.Equal(expected, result);
+    }
+}
---
+++ instructor/stringcalculator-wed-8-again/notes.md
@@ -0,0 +1,5 @@
+# String Calculator Kata
+
+From Roy Osherove
+
+[String Calculator 1](https://osherove.com/tdd-kata-1)
