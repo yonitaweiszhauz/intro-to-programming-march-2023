770cad3677c22d25982e8d3c95e8d714dae421f8
Jeffry Gonzalez <jeff@hypertheory.com>
2023-03-08 14:17:10 -0500

    Two Digits

--- instructor/stringcalculator-wed-8/StringCalculator.cs
+++ instructor/stringcalculator-wed-8/StringCalculator.cs
@@ -6,6 +6,18 @@ public class StringCalculator
 
     public int Add(string numbers)
     {
-        return numbers == "" ? 0 : int.Parse(numbers); // slime!
+        if(numbers == "")
+        {
+            return 0;
+        }
+        if(numbers.Contains(','))
+        {
+            var seperatorAt = numbers.IndexOf(',');
+            var lhs = int.Parse(numbers.Substring(0, seperatorAt));
+            var rhs = int.Parse(numbers.Substring(seperatorAt + 1));
+
+            return lhs + rhs;
+        }
+        return  int.Parse(numbers);
     }
 }
--- instructor/stringcalculator-wed-8/StringCalculatorTests.cs
+++ instructor/stringcalculator-wed-8/StringCalculatorTests.cs
@@ -24,4 +24,19 @@ public class StringCalculatorTests
 
         Assert.Equal(expected, result);
     }
+    [Theory]
+    [InlineData("4,4", 8)]
+    [InlineData("2,2", 4)]
+    [InlineData("193,7", 200)]
+    [InlineData("580,120", 700)]
+   
+    
+    public void TwoDigits(string numbers, int expected)
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add(numbers);
+
+        Assert.Equal(expected, result);
+    }
 }
