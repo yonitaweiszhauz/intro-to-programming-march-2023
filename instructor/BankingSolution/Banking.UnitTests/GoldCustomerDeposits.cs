95d2a49f5df77737a8a65ac3ed5d51aea493b768
Jeffry Gonzalez <jeff@hypertheory.com>
2023-03-08 11:24:16 -0500

    beginning bonus calculation

---
+++ instructor/BankingSolution/Banking.UnitTests/GoldCustomerDeposits.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Banking.UnitTests;
+
+public class GoldCustomerDeposits
+{
+    [Fact]
+    public void GoldCustomersGetABonusOnDeposits()
+    {
+        Assert.True(false); // Blow up!
+    }
+}
