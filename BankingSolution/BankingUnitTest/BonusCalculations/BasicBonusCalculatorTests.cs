24670d5903cb077397d9976a8ec98c2984b8ac2f
Yonita Weiszhauz <yonita.weiszhauz@gmail.com>
2023-03-09 11:15:19 -0500

    basic bonus calculator

---
+++ BankingSolution/BankingUnitTest/BonusCalculations/BasicBonusCalculatorTests.cs
@@ -0,0 +1,25 @@
+﻿using Banking.Domain;
+
+namespace BankingUnitTest.BonusCalculations
+{
+    public class BasicBonusCalculatorTests
+    {
+        // depsits that meet the criteria get a bonus
+        [Theory]
+        [InlineData(5000, 100, 10)]
+        [InlineData(5000, 200, 20)] // at boundary
+        [InlineData(4999.99, 100, 0)] // below boundary (add as needed)
+        public void DepositsGetBonusBasedOnBalance(decimal balance, decimal amount, decimal expectedBonus)
+        {
+            // Given
+            var bonusCalculator = new StandardBonusCalculator();
+
+            // When
+            decimal bonus = bonusCalculator.CalculateBankAccountDepositBonusFor(balance, amount);
+
+            // Then
+            Assert.Equal(expectedBonus, bonus);
+        }
+        // deposits that do not meet the criteria do not get a bonus
+    }
+}
--- BankingSolution/BankingUnitTest/GoldCustomerDeposits.cs
+++ BankingSolution/BankingUnitTest/GoldCustomerDeposits.cs
@@ -2,7 +2,7 @@
 
 public class GoldCustomerDeposits
 {
-    [Fact]
+    [Fact(Skip ="Get back to this after the bonus calculator is created")]
     public void GoldCustomersGetABonusOnDeposits()
     {
         Assert.True(false); // Blow up!
