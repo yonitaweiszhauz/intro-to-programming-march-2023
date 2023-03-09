899fcd27a82b8f19b90c4da1ae7c8e376412d275
Jeffry Gonzalez <jeff@hypertheory.com>
2023-03-09 16:29:40 -0500

    :shipit:

---
+++ instructor/OnCallDeveloperSolution/OnCallDeveloperApi.UnitTests/StandardBusinessClockTests.cs
@@ -0,0 +1,40 @@
+﻿
+using Moq;
+using OnCallDeveloperApi.Controllers;
+
+namespace OnCallDeveloperApi.UnitTests;
+
+public class StandardBusinessClockTests
+{
+    [Fact]
+    public void ReturnsTrueDuringBusinessHours()
+    {
+        var stubbedClock = new Mock<ISystemTime>();
+        stubbedClock.Setup(clock => clock.GetCurrent()).Returns(
+            new DateTime(1969, 4, 20, 09, 00, 00));
+        IProvideTheBusinessClock clock = new StandardBusinessClock(stubbedClock.Object);
+
+        Assert.True(clock.IsDuringBusinessHours());
+    }
+
+    [Fact]
+    public void ReturnsFalseBeforeBusinessHours()
+    {
+        var stubbedClock = new Mock<ISystemTime>();
+        stubbedClock.Setup(clock => clock.GetCurrent()).Returns(
+            new DateTime(1969, 4, 20, 08, 59, 59));
+        IProvideTheBusinessClock clock = new StandardBusinessClock(stubbedClock.Object);
+
+        Assert.False(clock.IsDuringBusinessHours());
+    }
+    [Fact]
+    public void ReturnsFalseAfterBusinessHours()
+    {
+        var stubbedClock = new Mock<ISystemTime>();
+        stubbedClock.Setup(clock => clock.GetCurrent()).Returns(
+            new DateTime(1969, 4, 20, 16, 00, 00));
+        IProvideTheBusinessClock clock = new StandardBusinessClock(stubbedClock.Object);
+
+        Assert.False(clock.IsDuringBusinessHours());
+    }
+}
---
+++ instructor/OnCallDeveloperSolution/OnCallDeveloperApi/Services/ISystemTime.cs
@@ -0,0 +1,14 @@
+﻿namespace OnCallDeveloperApi;
+
+public interface ISystemTime
+{
+    public DateTime GetCurrent();
+}
+
+public class SystemTime : ISystemTime
+{
+    public DateTime GetCurrent()
+    {
+        return DateTime.Now;
+    }
+}
