35f56dc362b986e8eded4e97a203728b9bbcb272
Jeffry Gonzalez <jeff@hypertheory.com>
2023-03-10 15:51:55 -0500

    done with week 1

---
+++ instructor/CSharpSyntaxSolution/CSharpSyntax/BankAccount.cs
@@ -0,0 +1,53 @@
+﻿
+
+using System.Reflection.Metadata;
+
+namespace CSharpSyntax;
+
+public class BankAccount
+{
+
+    private string _accountNumber = string.Empty;
+    private string _firstName = string.Empty;
+
+
+    internal BankAccount(string accountNumber, string firstName)
+    {
+        _accountNumber = accountNumber;
+        FirstName = firstName;
+    }
+
+
+    public string FirstName
+    {
+        get { return _firstName; }
+        private set { _firstName = value; }
+    }
+
+    public string LastName { get; private set; } = string.Empty;
+    public string GetAccountNumber()
+    {
+        return _accountNumber;
+    }
+}
+
+// Class that just does some work for me.//
+// Services
+
+public class AccountManager
+{
+
+    public BankAccount GetAccountById(string accountNumber)
+    {
+
+        // go find in the database
+        var account = new BankAccount(accountNumber, "Jeff");
+        
+        return account;
+    }
+
+    public void DoDeposit(BankAccount bankAccount, decimal amount)
+    {
+
+    }
+}
