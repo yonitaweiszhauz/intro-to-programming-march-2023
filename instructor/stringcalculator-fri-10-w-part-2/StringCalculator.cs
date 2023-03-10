a31e79cfea3275b71f3887b69f3d8b6111b45b1d
Jeffry Gonzalez <jeff@hypertheory.com>
2023-03-10 10:37:22 -0500

    logger is used

--- instructor/stringcalculator-fri-10-w-part-2/StringCalculator.cs
+++ instructor/stringcalculator-fri-10-w-part-2/StringCalculator.cs
@@ -1,11 +1,32 @@
 ﻿
+using System.Xml.Schema;
+
 namespace StringCalculator;
 
 public class StringCalculator
 {
+    private readonly ILogger _logger;
+
+    public StringCalculator(ILogger logger)
+    {
+        _logger = logger;
+    }
 
     public int Add(string numbers)
     {
-        return -42;
+        int total = numbers == "" ? 0 : numbers.Split(',', '\n') // ["1", "2"]
+                .Select(int.Parse) // [1,2]
+                .Sum();
+        
+
+        // WTCYWYH
+        _logger.Write(total.ToString());
+  
+        return total;
     }
+}
+
+public interface ILogger
+{
+    void Write(string message);
 }
---
+++ instructor/stringcalculator-fri-10-w-part-2/StringCalculatorInteractionTests.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace StringCalculator;
+
+public class StringCalculatorInteractionTests
+{
+
+    [Theory]
+    [InlineData("1,2,3", "6")]
+    [InlineData("42","42")]
+    [InlineData("", "0")]
+    public void ResultsAreLogged(string numbers, string expected)
+    {
+        // Given
+        var mockedLogger = new Mock<ILogger>();
+        var calculator = new StringCalculator(mockedLogger.Object);
+
+        // When
+        calculator.Add(numbers);
+
+        // Then
+        // Did the calculator call the Write method on the 
+        // logger with the value "6"
+        mockedLogger.Verify(m => m.Write(expected), Times.Once);
+
+
+    }
+}
