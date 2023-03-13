f212c9d898727fe1cedac2288690ec5cc0e84605
Jeffry Gonzalez <jeff@hypertheory.com>
2023-03-13 12:31:32 -0400

    before lunch

--- instructor/frontend/src/app/app.module.ts
+++ instructor/frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MastheadComponent } from './components/masthead/masthead.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { SupportComponent } from './components/support/support.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MastheadComponent,
+    DashboardComponent,
+    NavigationComponent,
+    SupportComponent
   ],
   imports: [
     BrowserModule,
