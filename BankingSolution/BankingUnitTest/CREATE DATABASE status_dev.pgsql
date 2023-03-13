0538bbb01e133e7608db076458e95dfb2d2d1e4b
Yonita Weiszhauz <yonita.weiszhauz@gmail.com>
2023-03-13 12:32:56 -0400

    before lunch monday 3/13

---
+++ BankingSolution/BankingUnitTest/CREATE DATABASE status_dev.pgsql
@@ -0,0 +1,1 @@
+CREATE DATABASE status_dev
--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SupportComponent } from './components/support/support.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'support',
+    component: SupportComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
