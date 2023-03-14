e88f36fd987def3255efeb46bc974d9049512b19
Yonita Weiszhauz <yonita.weiszhauz@gmail.com>
2023-03-14 12:34:52 -0400

    before lunch Tuesday 3/14

--- frontend/src/app/features/counter/counter.component.ts
+++ frontend/src/app/features/counter/counter.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
-  styleUrls: ['./counter.component.css']
+  styleUrls: ['./counter.component.css'],
 })
 export class CounterComponent {
+  current = 0;
+  increment() {
+    this.current += 1;
+  }
 
+  decrement() {
+    this.current -= 1;
+  }
 }
