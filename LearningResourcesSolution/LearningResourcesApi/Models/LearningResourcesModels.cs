36b5b3cd2f259ac1e25938fb10903c6a4af6e4f7
Yonita Weiszhauz <yonita.weiszhauz@gmail.com>
2023-03-15 14:44:43 -0400

    going to set up angular now

---
+++ LearningResourcesSolution/LearningResourcesApi/Models/LearningResourcesModels.cs
@@ -0,0 +1,6 @@
+﻿namespace LearningResourcesApi.Models; 
+
+public record LearningResourceSummaryItem(
+ string Id, string Name, string Description, string Link);
+
+public record LearningResourcesResponse(List<LearningResourceSummaryItem>Data);
